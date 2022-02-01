bf498aceb71278044a8b1ae34b44c9c2b8f0358a
Joshua <jkomwa@gmail.com>
2022-02-01 15:20:42 +0000

    Working on the account settings page. Stuck on css grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./css/App.css";
 import Sidebar from "./components/Sidebar";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import LiveStream from './pages/LiveStream';
+import Gallery from './pages/Gallery';
+import Setting from "./pages/Settings";
+import Logout from "./pages/Logout";
 
 
 function App() {
@@ -10,7 +15,11 @@ function App() {
     <Router>
       <Sidebar/>
       <Routes>
-        <Route path='/'/>
+        <Route path='/' exact element={<Home/>}/>
+        <Route path='/livestream' element={<LiveStream/>}/>
+        <Route path='/gallery' element={<Gallery/>}/>
+        <Route path='/settings'element={<Setting/>}/>
+        <Route path='/logout'element={<Logout/>}/>
       </Routes>
 
     </Router>
