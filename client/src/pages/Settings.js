57d3f9c75f7674909de2658b5530fb2809cc4f54
Joshua <jkomwa@gmail.com>
2022-02-02 16:15:12 +0000

    Account settings. Working on the button on the page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,18 +1,60 @@
 import React from 'react';
+import *as AiIcons from "react-icons/ai";
 
 function Setting() {
   return(<div className='settings'>
        <h1>Account Settings</h1>
+       <h3>General settings</h3>
 
-       <div className='Wrapper'>
-         <div class="One">Profile Details</div>
-         <div class="two">Change password</div>
-         <div class="three">Close account</div>
+       
 
+       <div className='wrapper'>
+         <div className="w1"> <h4 className='profilePic'> <AiIcons.AiOutlineUser/>Profile Details</h4>
+
+
+            <div className='uname'> 
+            <label for="username"><h7>Username</h7> </label>
+            <input type="text" id="username" name="username"></input>
+
+            </div>
+
+            <div className='email'>
+              <label for="email"><h7>Email</h7></label>
+              <input type="text" id="email" name="email"></input>
+              
+              </div>
+            
+            </div>
+
+
+         <div className="w2" >
+           <h4 className='iconsC'> <AiIcons.AiFillLock/> Change password</h4> 
+
+
+            <div className='textpass'>
+            <h9>You can permanently delete or temporarily freeze your account</h9>
+
+            </div>
+
+            <div className='password'>
+              <button type="button" onclick="alert(you clicked me)"><h2>Change Password</h2></button>
+            </div>
+
+
+         
+         </div>
+         <div className="w3">
+           <h4 className='iconsC'>Close account</h4>
+              <div className='textclose'>
+              <h9>You can permanently delete or temporarily freeze your account</h9>
+              </div>
+         </div>
 
        </div>
+
+      
 
    </div>)
 }
 
-export default Setting;
+export default Setting; 
