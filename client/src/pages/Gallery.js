dee96288a861af2b22577b302dc748d248f4fa9d
Joshua <jkomwa@gmail.com>
2022-02-05 01:33:13 +0000

    Not done yet. Just pushing for fun

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -3,6 +3,35 @@ import React from 'react';
 function Gallery() {
     return(<div className='gallery'>
     <h1>Gallery</h1>
+    <div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400"></img>
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_forest.jpg">
+    <img src="img_forest.jpg" alt="Forest" width="600" height="400"></img>
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_lights.jpg">
+    <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400"></img>
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_mountains.jpg">
+    <img src="img_mountains.jpg" alt="Mountains" width="600" height="400"></img>
+  </a>
+  <div class="desc">Add a description of the image here</div>
+
+</div>
+
 
 </div>)
 }
