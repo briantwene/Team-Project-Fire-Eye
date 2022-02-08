0223472a4e7b6ffdddbabc87ad8f9a6c7f76e411
Joshua <jkomwa@gmail.com>
2022-02-08 15:57:18 +0000

    Dashboard Change

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -6,55 +6,66 @@ function Gallery() {
       <h1>Gallery</h1>
       <div className="gallery__items">
         <div className="gallery__items--item">
-          <a target="_blank" href="img_5terre.jpg">
+          
             <img
               src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
               alt="Cinque Terre"
-              width="600"
-              height="400"
+            
             ></img>
-          </a>
+         
           <div className="desc">Add a description of the image here</div>
         </div>
 
         <div className="gallery__items--item">
-          <a target="_blank" href="img_forest.jpg">
+         
             <img
               src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
               alt="Forest"
-              width="600"
-              height="400"
+             
             ></img>
-          </a>
+         
           <div className="desc">Add a description of the image here</div>
         </div>
 
         <div className="gallery__items--item">
-          <a target="_blank" href="img_lights.jpg">
+     
             <img
               src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
               alt="Northern Lights"
-              width="600"
-              height="400"
+              
             ></img>
-          </a>
+          
           <div className="desc">Add a description of the image here</div>
         </div>
 
         <div className="gallery__items--item">
-          <a target="_blank" href="img_mountains.jpg">
+         
             <img
               src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
               alt="Mountains"
-              width="600"
-              height="400"
+            
             ></img>
-          </a>
+
+          <div className="desc">Add a description of the image here</div>
+
+        </div>
+        <div className="gallery__items--item">
+         
+            <img
+              src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              alt="Forest"
+             
+            ></img>
+         
           <div className="desc">Add a description of the image here</div>
         </div>
       </div>
     </div>
   );
 }
+
+
+
+
 
 export default Gallery;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,31 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-  return(<div className='home'>
-       <h1>Dashboard</h1>
+  return (
+    <div className="home">
+      <h1>Dashboard</h1>
 
+      <div className=" container-space home__display">
+        <div className="home__display--item pictures">
+          <p>Pictures</p>
+          <h2>20</h2>
+        </div>
 
-       <div className ='rectangle'>
-
-          <div class="footage" >  <h2>Pictures </h2></div>
-             
-             
-            <div class="footage"><h2>Videos</h2> </div>
-                  
-        
-       </div>
-
-
-
-
-
-
-
-
-
-   </div>)
-
-  
+        <div className="home__display--item videos">
+          <p>Videos</p>
+          <h2>5</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
