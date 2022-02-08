80798c66ae26dc40cd5c9b4b5fa27cd5ae303ae9
Joshua <jkomwa@gmail.com>
2022-02-08 20:12:48 +0000

    Updated Gallery and Dashboard

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -6,66 +6,107 @@ function Gallery() {
       <h1>Gallery</h1>
       <div className="gallery__items">
         <div className="gallery__items--item">
-          
-            <img
-              src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="Cinque Terre"
-            
-            ></img>
-         
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Cinque Terre"
+          ></img>
+
           <div className="desc">Add a description of the image here</div>
         </div>
 
         <div className="gallery__items--item">
-         
-            <img
-              src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="Forest"
-             
-            ></img>
-         
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
           <div className="desc">Add a description of the image here</div>
         </div>
 
         <div className="gallery__items--item">
-     
-            <img
-              src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="Northern Lights"
-              
-            ></img>
-          
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Northern Lights"
+          ></img>
+
           <div className="desc">Add a description of the image here</div>
         </div>
 
         <div className="gallery__items--item">
-         
-            <img
-              src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="Mountains"
-            
-            ></img>
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Mountains"
+          ></img>
 
           <div className="desc">Add a description of the image here</div>
-
         </div>
         <div className="gallery__items--item">
-         
-            <img
-              src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="Forest"
-             
-            ></img>
-         
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
+          <div className="desc">Add a description of the image here</div>
+        </div>
+        <div className="gallery__items--item">
+          <img
+            src="https://images.unsplash.com/photo-1644027616320-b378fc57f78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="Forest"
+          ></img>
+
           <div className="desc">Add a description of the image here</div>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-
 
 export default Gallery;
