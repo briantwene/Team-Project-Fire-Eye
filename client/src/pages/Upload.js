a304d92fb65e66e93f02f6052b6ca23b6afb0b68
Joshua <jkomwa@gmail.com>
2022-02-13 01:38:36 +0000

    Update Page v2

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,23 +1,64 @@
-import React from "react";
+// import { response } from "express";
+import React, {useState} from "react";
 
 function Upload() {
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+
+  const changeHandler = (event) =>{
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  }
+
+  const handleSUbmission = () =>{
+    const formData = new FormData();
+    formData.append('File', selectedFile);
+
+    fetch(
+      'https://freeimage.host/api/1/upload?key=<App.js>',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    )
+
+      .then((response) => response.json())
+      .then((result) =>{
+        console.log('Success:', result);
+      })
+
+      // .catch((error)) => {
+      //   console.error('Error:', error);
+      // }
+
+  };
+
   return (
-    <>
-      <div className="Up">
-        <h1>Upload </h1>
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {isFilePicked ?(
+          <div>
+            <p>Filename: {selectedFile.name}</p>
+            <p>Filetype: {selectedFile.type}</p>
+            <p>Size in bytes: {selectedFile.size}</p>
+
+            <p>
+              lastModifiedDate: {' '}
+              {selectedFile.lastModifiedDate.toLocaleDateString()}
+            </p>
+
+          </div>
+      ):(
+        <p>Select a file to show details</p>
+      )
+      
+    }
+      <div>
+        <button onClick={handleSUbmission}>Submit</button>
       </div>
+    </div>
 
-
-
-      <div className= "select">
-
-        <button className="selectUp" type="button">
-              <h2>Import From</h2>
-            </button>
-        {/* <h4>Import From</h4> */}
-
-        </div>
-    </>
   );
 }
 
