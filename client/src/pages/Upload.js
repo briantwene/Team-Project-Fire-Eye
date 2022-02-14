7000e20d90e32c7368e3ba076e90563d80d4cf59
Joshua <jkomwa@gmail.com>
2022-02-14 12:56:27 +0000

    sum merge

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,64 +1,59 @@
 // import { response } from "express";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { files } from "../components/foldertree";
 
 function Upload() {
-    const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
 
-
-  const changeHandler = (event) =>{
-    setSelectedFile(event.target.files[0]);
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files);
+    console.log(event.target.files);
     setIsFilePicked(true);
-  }
+  };
 
-  const handleSUbmission = () =>{
+  const handleSUbmission = () => {
     const formData = new FormData();
-    formData.append('File', selectedFile);
 
-    fetch(
-      'https://freeimage.host/api/1/upload?key=<App.js>',
-      {
-        method: 'POST',
-        body: formData,
-      }
-    )
+    for (let i = 0; i < selectedFile.length; i++) {
+      formData.append(`File[${i}]`, selectedFile[i]);
+    }
+    // formData.append('File', selectedFile);
 
+    fetch("/nas/upload", {
+      method: "POST",
+      body: formData,
+    })
       .then((response) => response.json())
-      .then((result) =>{
-        console.log('Success:', result);
-      })
+      .then((result) => {
+        console.log("Success:", result);
+      });
 
-      // .catch((error)) => {
-      //   console.error('Error:', error);
-      // }
-
+    // .catch((error)) => {
+    //   console.error('Error:', error);
+    // }
   };
 
   return (
     <div>
-      <input type="file" name="file" onChange={changeHandler} />
-      {isFilePicked ?(
-          <div>
-            <p>Filename: {selectedFile.name}</p>
-            <p>Filetype: {selectedFile.type}</p>
-            <p>Size in bytes: {selectedFile.size}</p>
+      {/* <form method="post" enctype="multipart/form-data"> */}
+      <input type="file" name="files" onChange={changeHandler} multiple />
+      {/* </form> */}
+      {isFilePicked ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
 
-            <p>
-              lastModifiedDate: {' '}
-              {selectedFile.lastModifiedDate.toLocaleDateString()}
-            </p>
-
-          </div>
-      ):(
+          <p>lastModifiedDate: {selectedFile.lastModifiedDate}</p>
+        </div>
+      ) : (
         <p>Select a file to show details</p>
-      )
-      
-    }
+      )}
       <div>
         <button onClick={handleSUbmission}>Submit</button>
       </div>
     </div>
-
   );
 }
 
